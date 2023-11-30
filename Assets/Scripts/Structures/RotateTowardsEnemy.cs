d9c93c0191582ddd037ea8739d8462396c48d1ca
eze <69222000+quiellll@users.noreply.github.com>
2023-11-30 18:27:55 +0100

    testing

---
+++ Assets/Scripts/Structures/RotateTowardsEnemy.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateTowardsEnemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            Debug.Log("Enemigo detectado");
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("pim");
+    }
+}
