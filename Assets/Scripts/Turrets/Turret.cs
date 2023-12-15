6ba40074a548e2b7d8ccf59f0a4132527cf7940d
eze <69222000+quiellll@users.noreply.github.com>
2023-12-15 19:55:14 +0100

    Código comentario torretas
    Comentado el código de todo lo que incluye a las torretas y proyectiles (a día de hoy)

--- Assets/Scripts/Turrets/Turret.cs
+++ Assets/Scripts/Turrets/Turret.cs
@@ -6,12 +6,14 @@ using UnityEngine;
 
 public class Turret : ScriptableObject
 {
+    // Prefab de la torreta
     public GameObject Prefab { get => _turretPrefab; }
+    // Prefab de placeholder
     public GameObject Dummy { get => _dummyPrefab; }
+    // Prefab del proyectil a disparar
     public ATurretProjectile ProjectilePrefab { get => _projectilePrefab; }
 
     [SerializeField] private GameObject _turretPrefab;
     [SerializeField] private GameObject _dummyPrefab;
-
     [SerializeField] private ATurretProjectile _projectilePrefab;
 }
