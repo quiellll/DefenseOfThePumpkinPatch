f3195ed3887930d47c0cd0f80b7cc9dd896013bf
Miguel Rico Feo <miguelrico2031@gmail.com>
2024-01-09 13:35:14 +0100

    script de cambio de fuente

---
+++ Assets/FontChange.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[ExecuteInEditMode]
+public class FontChange : MonoBehaviour
+{
+    [SerializeField] private TMP_FontAsset _fontAsset;
+    private void Update()
+    {
+        if (_fontAsset == null) return;
+        foreach(var t in FindObjectsOfType<TextMeshProUGUI>(true))
+        {
+            t.font = _fontAsset;
+        }
+    }
+}
